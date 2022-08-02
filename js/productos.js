ddff7abf82b401e6cc37c14c3fe313d5c11cb87b
battagliajose <battagliajose@gmail.com>
2022-08-02 15:56:38 -0300

    Inicio de CSS Responsive

---
+++ js/productos.js
@@ -0,0 +1,5 @@
+var btnAgregarProducto = document.querySelector("#btnAgregarProducto");
+
+btnAgregarProducto.addEventListener("click", function() {
+    window.open("agregarproducto.html","_self");
+})
