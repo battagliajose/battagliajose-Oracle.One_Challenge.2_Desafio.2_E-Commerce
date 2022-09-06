885318fa59013ba3a5d6650407831380c942b865
battagliajose <86583581+battagliajose@users.noreply.github.com>
2022-09-05 23:57:01 -0300

    Lógica del botón Consolas

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import { productsController } from "./controllers/productos-controller.js";
 
 var btnLogin = document.querySelector("#btnLogin");
+var btnVerConsolas = document.querySelector("#btnVerConsolas")
 var inputFilter = document.querySelector("[data-inputfilter]");
 
 inputFilter.addEventListener("input", function () { 
@@ -9,6 +10,11 @@ inputFilter.addEventListener("input", function () {
 
 btnLogin.addEventListener("click", function() {
     window.open("login.html","_self");
+});
+
+btnVerConsolas.addEventListener("click", function() {
+    inputFilter.value = "Consola";
+    productsController.render(inputFilter.value)
 });
 
 productsController.render(null, 6);
